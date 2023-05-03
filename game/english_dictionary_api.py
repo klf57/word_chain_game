6e845befbb46f94ea4d645206880fe14ebbcbab5
Kenn Leen Fulgencio <klf57@uclive.ac.nz>
2023-05-03 20:26:12 +1200

    added english api

---
+++ game/english_dictionary_api.py
@@ -0,0 +1,20 @@
+"""Contains logic for connectin to an english language api used for the game so it can access words.
+It uses the api from https://rapidapi.com/uiopuiop3385/api/random-word-api"""
+import requests
+import os
+
+"""Always returns a valid English word that will start with given letter."""
+def get_word_matching_letter(first_letter):
+
+    url = 'https://random-word-api.p.rapidapi.com'.format(first_letter)
+
+    #required to gain access to api.
+    headers = {
+        "X-RapidAPI-Key": os.getenv("API_KEY"),
+        "X-RapidAPI-Host": "random-word-api.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+
+
+
